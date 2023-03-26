02244b199591db87d4c233160ffe0897a3ae7db1
Yottaphy <jorge13romero@gmail.com>
2023-03-26 18:23:01 +0300

    commented the program

--- plunger.py
+++ plunger.py
@@ -6,20 +6,24 @@ import scienceplots
 import sys
 import locale
 
+# Style options
 plt.style.use("science")
 fig, ax = plt.subplots(figsize=(16, 8))
 
+# First number gives you the set, second and third gives you the initial and final runs, respectively.
 runset = int(sys.argv[1])
 run_init = int(sys.argv[2])
 run_fin = int(sys.argv[3])
 
-time = []
-voltage = []
-error = []
-distance = []
-runends = []
+# Initiate empty vectors
+time = []  # Time vector (not working yet)
+voltage = []  # Voltage points (first column in the file)
+error = []  # Error points (second column)
+distance = []  # Distance points (last column)
+runends = []  # A vector to store the points at which each run ends.
 runstart = 0
 
+# For every run in the range, open the corresponding file and then extend the global vectors
 for run in range(run_init, run_fin + 1):
     filename = "Set_" + str(runset) + "/regelungsrun_1." + str("{:04d}".format(run))
     v, e, d = np.genfromtxt(filename, unpack=True)
@@ -32,10 +36,12 @@ for run in range(run_init, run_fin + 1):
     runstart = len(voltage)
 
 
+# Chose max and min for plotting
 distance = [x + 19.7 for x in distance]
 miny = min(distance) - min(distance) % 5
 maxy = min(max(distance) + (5 - max(distance) % 5), 50)
 
+# Add vertical lines at the last point of every run
 ax.vlines(
     runends,
     ymin=miny,
@@ -44,6 +50,7 @@ ax.vlines(
     linestyle="-",
 )
 
+# For every run, add a label under the x-axis and slightly to the right of the drawn line
 for run in range(run_init, run_fin + 1):
     if run == run_init:
         ax.text(500, miny - 2.5, "Run " + str(run), rotation="vertical")
@@ -52,6 +59,7 @@ for run in range(run_init, run_fin + 1):
             runends[run - 4] + 500, miny - 2.5, "Run " + str(run), rotation="vertical"
         )
 
+# Plot plunger distances
 distcol = "C0"
 ax.plot(distance, color=distcol)
 ax.set_ylim(miny, maxy)
@@ -62,16 +70,20 @@ ax.set_ylabel("Plunger opening [µm]")
 ax.grid(which="both")
 
 ax.set_xlim(0, len(voltage))
-ax.set_xticks([])
+ax.set_xticks([])  # Remove axis ticks
 
-# voltcol = "C3"
-# ax2 = ax.twinx()
-# ax2.plot(voltage, color=voltcol)
-# ax2.set_ylabel("Plunger Voltage [V]")
-# ax2.spines["right"].set_color(voltcol)
-# ax2.yaxis.label.set_color(voltcol)
-# ax2.tick_params(axis="y", colors=voltcol)
+# Plot voltages on the same plot as before
+"""
+voltcol = "C3"
+ax2 = ax.twinx()
+ax2.plot(voltage, color=voltcol)
+ax2.set_ylabel("Plunger Voltage [V]")
+ax2.spines["right"].set_color(voltcol)
+ax2.yaxis.label.set_color(voltcol)
+ax2.tick_params(axis="y", colors=voltcol)
+"""
 
+# Save the fig as a pdf with an automatic name
 plt.savefig(
     "outputs/PlungerDistance_Set"
     + str(runset)
